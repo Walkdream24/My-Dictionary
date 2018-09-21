dfa43c912e9899a791e22f21f9bb3c32e6f68009
github Walkdream24 <walkdream24@gmail.com>
2018-09-21 22:34:11 +0900

    Initial Commit

---
+++ My Dictionary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  My Dictionary
+//
+//  Created by 中重歩夢 on 2018/09/21.
+//  Copyright © 2018年 Ayumu Nakashige. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
