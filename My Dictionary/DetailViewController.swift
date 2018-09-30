28ee2b9fad2830306a52ecad7b68491ea4e195ab
github Walkdream24 <walkdream24@gmail.com>
2018-09-30 15:37:46 +0900

    Add Picture change Button

--- My Dictionary/DetailViewController.swift
+++ My Dictionary/DetailViewController.swift
@@ -9,18 +9,20 @@
 import UIKit
 import RealmSwift
 
-class DetailViewController: UIViewController {
+class DetailViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     var getWord: String!
     var getDetail: String!
     var getImageData: Data!
     var getId: Int!
+    var pickImage: UIImage!
    
     
     
     @IBOutlet weak var wordLabel: UILabel!
     @IBOutlet weak var detailTextView: UITextView!
     @IBOutlet weak var detailImage: UIImageView!
+
     
     
     override func viewDidLoad() {
@@ -77,16 +79,28 @@ class DetailViewController: UIViewController {
     }
     
     @IBAction func updateButton(_ sender: Any) {
-        
         detailTextView.isEditable = false
-        let realm = try! Realm()
-        let editObj = myWordObject()
-        editObj.wordName = getWord
-        editObj.imageData = getImageData
-        editObj.wordDetail = detailTextView.text
-        editObj.id = getId
-        try! realm.write {
-            realm.add(editObj, update: true)
+        if detailImage.image == pickImage {
+            let pickData = pickImage!.pngData()
+            let realm = try! Realm()
+            let editObj = myWordObject()
+            editObj.imageData = pickData!
+            editObj.wordName = getWord
+            editObj.wordDetail = detailTextView.text
+            editObj.id = getId
+            try! realm.write {
+                realm.add(editObj, update: true)
+            }
+        }else{
+            let realm = try! Realm()
+            let editObj = myWordObject()
+            editObj.wordName = getWord
+            editObj.imageData = getImageData
+            editObj.wordDetail = detailTextView.text
+            editObj.id = getId
+            try! realm.write {
+                realm.add(editObj, update: true)
+            }
         }
         let alert = UIAlertController(title: "Update", message: "編集を更新しました", preferredStyle: .alert)
         let buttonOk = UIAlertAction(title: "OK", style: .default, handler: nil)
@@ -94,7 +108,23 @@ class DetailViewController: UIViewController {
         
         self.present(alert, animated: true, completion: nil)
         }
-      
+    @IBAction func imageChangeButton(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            let pickerView = UIImagePickerController()
+            pickerView.sourceType = .photoLibrary
+            pickerView.delegate = self
+            self.present(pickerView, animated: true)
+            
+        }
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[.originalImage] as! UIImage
+        pickImage = image
+        self.detailImage.image = pickImage
+        self.dismiss(animated: true, completion: nil)
+        
+    }
+    
    
       
 }
